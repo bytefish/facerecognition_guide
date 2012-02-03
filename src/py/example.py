b0b16c979071c28d144a6a75e604330bdede885f
Philipp Wagner <bytefish@gmx.de>
2012-02-03 01:37:37 +0100

    example corrected, still takes some time to include it in the guide.

--- src/py/example.py
+++ src/py/example.py
@@ -1,33 +1,34 @@
-import util, subspace
-
-from pylab import *
+import util, subspace, model
+import numpy as np
+import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
 def create_font(fontname='Tahoma', fontsize=10):
 	return { 'fontname': fontname, 'fontsize':fontsize }
 
-def subplot(title, images, rows, cols, title2="subplot", colormap=cm.gray, ticks_visible=True, filename="plot.pdf"):
+def subplot(title, images, rows, cols, sptitle="subplot", colormap=cm.gray, ticks_visible=True, filename="plot.pdf"):
 	fig = plt.figure()
 	# main title
 	fig.text(.5, .95, title, horizontalalignment='center') 
 	for i in xrange(len(images)):
 		ax0 = fig.add_subplot(rows,cols,(i+1))
-		setp(ax0.get_xticklabels(), visible=False)
-		setp(ax0.get_yticklabels(), visible=False)
-		#print title,i
-		#print "%s #%d" % (title2, i)
-		#title("%s #%d" % (title2, i), create_font('Tahoma',10))
-		imshow(np.asarray(images[i]), cmap=colormap)
+		plt.setp(ax0.get_xticklabels(), visible=False)
+		plt.setp(ax0.get_yticklabels(), visible=False)
+		plt.title("%s #%d" % (sptitle, (i+1)), create_font('Tahoma',10))
+		plt.imshow(np.asarray(images[i]), cmap=colormap)
 	fig.savefig(filename, format="pdf")
 
 [X,y] = util.read_images("/home/philipp/facerec/data/yalefaces_recognition")
-print len(X)
+# Fisherfaces and prediction...
+from model import EigenfacesModel, FisherfacesModel
+model = FisherfacesModel(X[1:],y[1:])
+print model.predict(X[0])
+# Plot the fisherfaces
 [D, W, mu] = subspace.fisherfaces(util.asRowMatrix(X), y)
-
+# ... turn them into normalized 2D matrices first
 F = []
-print min(W.shape[1], 16)
 for i in xrange(min(W.shape[1], 16)):
 	ev = W[:,i].reshape(X[0].shape)
 	F.append(util.normalize(ev,0,255))
-
-subplot("Fisherfaces", F, 4, 4, filename="/home/philipp/test.pdf")
+# ... and plot them
+subplot("Fisherfaces", F, 4, 4, sptitle="Fisherface", colormap=cm.jet, filename="/home/philipp/test.pdf")
