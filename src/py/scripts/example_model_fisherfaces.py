e6936488cfa6dea0c0dcc078ad62c4422babf3bf
Philipp Wagner <bytefish@gmx.de>
2012-07-27 21:38:38 +0200

    You can now pass the image folder to the scripts. Probably makes it easier to run these for beginners.

--- src/py/scripts/example_model_fisherfaces.py
+++ src/py/scripts/example_model_fisherfaces.py
@@ -6,9 +6,16 @@ import numpy as np
 # import tinyfacerec modules
 from tinyfacerec.util import read_images
 from tinyfacerec.model import FisherfacesModel
-# read images
-[X,y] = read_images("/home/philipp/facerec/data/yalefaces_recognition")
-# compute the eigenfaces model
-model = FisherfacesModel(X[1:], y[1:])
-# get a prediction for the first observation
-print "expected =", y[0], "/", "predicted =", model.predict(X[0])
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 2:
+        print "USAGE: example_model_fisherfaces.py </path/to/images>"
+        sys.exit()
+    
+    # read images
+    [X,y] = read_images(sys.argv[1])
+    # compute the eigenfaces model
+    model = FisherfacesModel(X[1:], y[1:])
+    # get a prediction for the first observation
+    print "expected =", y[0], "/", "predicted =", model.predict(X[0])
